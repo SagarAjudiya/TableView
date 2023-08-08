399d9cedc2d8c4559778e589502541168eb1eac9
Sagar <sagarajudiya2002@gmail.com>
2023-08-08 14:59:48 +0530

    Stepper with tableview

--- WebService/WebService/Controller/SelectVC.swift
+++ WebService/WebService/Controller/SelectVC.swift
@@ -44,6 +44,7 @@ extension SelectVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "SelectTblCell", for: indexPath) as? SelectTblCell else { return UITableViewCell() }
         cell.delegate = self
+        cell.stepperDelegate = self
         cell.indexPath = indexPath
         cell.configcell(data: SelectModel.list[indexPath.section].items[indexPath.row])
         return cell
@@ -68,10 +69,10 @@ extension SelectVC: UITableViewDelegate, UITableViewDataSource {
 extension SelectVC: RowButtonProtocol {
     
     func rowButtonClicked(_ indexPath: IndexPath) {
-        SelectModel.list[indexPath.section].items[indexPath.row].isSelect.toggle()
-        SelectModel.list[indexPath.section].isSelect = SelectModel.list[indexPath.section].items.filter({$0.isSelect == true}).count != 0
-        tblSelect.reloadRows(at: [indexPath], with: .fade)
-        tblSelect.reloadSections(IndexSet(integer: indexPath.section), with: .fade)
+//        SelectModel.list[indexPath.section].items[indexPath.row].isSelect.toggle()
+//        SelectModel.list[indexPath.section].isSelect = SelectModel.list[indexPath.section].items.filter({$0.isSelect == true}).count != 0
+//        tblSelect.reloadRows(at: [indexPath], with: .fade)
+//        tblSelect.reloadSections(IndexSet(integer: indexPath.section), with: .fade)
     }
 
 }
@@ -79,9 +80,24 @@ extension SelectVC: RowButtonProtocol {
 extension SelectVC: HeaderButtonProtocol {
     
     func headerButtonClicked(_ section: Int) {
-        SelectModel.list[section].isSelect.toggle()
-        _ = SelectModel.list[section].items.compactMap({$0.isSelect = SelectModel.list[section].isSelect})
-        tblSelect.reloadSections(IndexSet(integer: section), with: .fade)
+//        SelectModel.list[section].isSelect.toggle()
+//        _ = SelectModel.list[section].items.compactMap({$0.isSelect = SelectModel.list[section].isSelect})
+//        tblSelect.reloadSections(IndexSet(integer: section), with: .fade)
+    }
+    
+}
+
+extension SelectVC: StepperDelegate {
+    
+    func stepperClicked(_ value: Double, _ indexPath: IndexPath) {
+        print(indexPath)
+        
+        SelectModel.list[indexPath.section].items[indexPath.row].count = value
+        SelectModel.list[indexPath.section].items[indexPath.row].isSelect = SelectModel.list[indexPath.section].items[indexPath.row].count > 0
+        SelectModel.list[indexPath.section].isSelect = SelectModel.list[indexPath.section].items.filter({$0.isSelect == true}).count != 0
+        
+        tblSelect.reloadRows(at: [indexPath], with: .fade)
+        tblSelect.reloadSections(IndexSet(integer: indexPath.section), with: .fade)
     }
     
 }
--- WebService/WebService/Model/SelectModel.swift
+++ WebService/WebService/Model/SelectModel.swift
@@ -11,15 +11,20 @@ class SelectModel {
     
     var isSelect: Bool = false
     var title: String
+    var count: Double = 0.0
     var items: [SelectModel] = []
     
-    init(title: String, items: [SelectModel] = []) {
+    init(title: String, count: Double = 0.0, items: [SelectModel] = []) {
         self.title = title
         self.items = items
+        self.count = count
     }
     
-    static let list: [SelectModel] = [.init(title: "Header 1", items: [.init(title: "Row 1"), .init(title: "Row 2"), .init(title: "Row 3")]),
-                                      .init(title: "Header 2", items: [.init(title: "Row 1"), .init(title: "Row 2"), .init(title: "Row 3")]),
-                                      .init(title: "Header 3", items: [.init(title: "Row 1"), .init(title: "Row 2"), .init(title: "Row 3")])]
+    static let list: [SelectModel] = [.init(title: "Header 1", items: [.init(title: "Row 1", count: 0), .init(title: "Row 2", count: 0), .init(title: "Row 3")]),
+                                      .init(title: "Header 2", items: [.init(title: "Row 1", count: 0), .init(title: "Row 2", count: 0), .init(title: "Row 3", count: 0)]),
+                                      .init(title: "Header 3", items: [.init(title: "Row 1", count: 0), .init(title: "Row 2", count: 0), .init(title: "Row 3", count: 0)]),
+                                      .init(title: "Header 4", items: [.init(title: "Row 1", count: 0), .init(title: "Row 2", count: 0), .init(title: "Row 3", count: 0)]),
+                                      .init(title: "Header 5", items: [.init(title: "Row 1", count: 0), .init(title: "Row 2", count: 0), .init(title: "Row 3", count: 0)]),
+                                      .init(title: "Header 6", items: [.init(title: "Row 1", count: 0), .init(title: "Row 2", count: 0), .init(title: "Row 3", count: 0)])]
     
 }
